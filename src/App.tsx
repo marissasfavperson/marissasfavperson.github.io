5d25dfa29199945f517add6d2263f9ce2afb397b
unknown <you@example.com>
2024-02-03 15:34:03 +0000

    add

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default function App() {
       {yesPressed ? (
         <>
         <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-        <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+        <div className="text-4xl font-bold my-4">Ok yay I love you Marissa :)</div>
         </>
       ) : (
         <>
